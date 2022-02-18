cc98233723ed5ff089a62bd49e7591b68eee1f77
ib1404 <92749085+ib1404@users.noreply.github.com>
2022-02-18 17:01:04 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,27 +13,30 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Quelle est la valeur de a ?");
-        int a = scanner.nextInt();
+        double a = scanner.nextDouble();
 
         System.out.println("Quelle est la valeur de b ?");
-        int b = scanner.nextInt();
+        double b = scanner.nextDouble();
 
         System.out.println("Quelle est la valeur de c ?");
-        int c = scanner.nextInt();
+        double c = scanner.nextDouble();
 
-        int delta = (double) (Math.pow(b, 2) - 4 * a * c);
+        int delta = (int) (Math.pow(b, 2) - 4 * a * c);
 
         if (delta == 0){
-            int x0 = -(b/(2*a));
+            double x0 = -(b/(2*a));
             System.out.println("Ce polynome a une racine double x0 = " + x0);
 
         } else if (delta > 0){
-            int x1 = ((-b-Math.sqrt(delta))/(2*a));
-            int x2 = ((-b+Math.sqrt(delta))/(2*a));
+            double x1 = ((-b-Math.sqrt(delta))/(2*a));
+            double x2 = ((-b+Math.sqrt(delta))/(2*a));
             System.out.println("Ce polynome a deux racines simples x1 = " + x1 + " et x2 = " + x2);
 
         }else if (delta < 0) {
-            System.out.println("Ce polynome n’a pas de racine reelle");
+            double r = -(b/(2*a));
+            double d1 = (Math.sqrt(-delta)/(2*a));
+            System.out.println("Ce polynome n’a pas de racine reelle, r1 = "+r+" + "+d1+"i et r2 = "+r+" - "+d1+"i");
+
         }
     }
 }
